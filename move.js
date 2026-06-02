a4b0a19151e68264c6b6263f0e5492f2953f14ac
25zDK <251zs01221@student.zen.ac.jp>
2026-06-02 22:10:47 +0900

    JavaScriptファイルの改良1

--- move.js
+++ move.js
@@ -3,24 +3,21 @@ let y = 0;
 let easing = 0.25;
 
 function setup() {
-  // 画面全体（ブラウザの幅と高さ）にキャンバスを作る
   createCanvas(windowWidth, windowHeight);
+  canvas.id('p5-canvas');
 }
 
 function draw() {
-  background(241); // 背景を毎回塗りつぶす（残像を消す）
+  background(241);
   
-  // イージング（滑らかな追従）の計算
   x += (mouseX - x) * easing;
   y += (mouseY - y) * easing;
   
-  // 図形の描画（ここでは青い円）
   noStroke();
   fill(232,153,104);
   circle(x, y, 16);
 }
 
-// ユーザーがブラウザのサイズを変えたときに背景も追従させる設定
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
